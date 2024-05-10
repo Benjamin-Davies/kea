60587d0de4a6fed5fc1c5c09f768079e9b8edd76
Benjamin Davies <bentendavies@gmail.com>
2024-05-10 22:20:17 +1200

    Add ensureWhitespace and ensureNewline helpers

--- Sources/Rewriters/Pad.swift
+++ Sources/Rewriters/Pad.swift
@@ -5,23 +5,28 @@ class Pad: SyntaxRewriter {
 
     override func visit(_ node: FunctionDeclSyntax) -> DeclSyntax {
         var node = node
-        node.funcKeyword.trailingTrivia = .space
-        node.body?.leadingTrivia = .space
+        node.funcKeyword.trailingTrivia.ensureWhitespace()
+        node.body?.leadingTrivia.ensureWhitespace()
         return super.visit(node)
     }
 
     override func visit(_ node: ImportDeclSyntax) -> DeclSyntax {
-        super.visit(node.with(\.importKeyword.trailingTrivia, .space))
+        var node = node
+        node.importKeyword.trailingTrivia.ensureWhitespace()
+        return super.visit(node)
     }
 
     override func visit(_ node: StructDeclSyntax) -> DeclSyntax {
-        super.visit(node
-            .with(\.structKeyword.trailingTrivia, .space)
-            .with(\.memberBlock.leadingTrivia, .space))
+        var node = node
+        node.structKeyword.trailingTrivia.ensureWhitespace()
+        node.memberBlock.leadingTrivia.ensureWhitespace()
+        return super.visit(node)
     }
 
     override func visit(_ node: VariableDeclSyntax) -> DeclSyntax {
-        super.visit(node.with(\.bindingSpecifier.trailingTrivia, .space))
+        var node = node
+        node.bindingSpecifier.trailingTrivia.ensureWhitespace()
+        return super.visit(node)
     }
 
     // Expressions
@@ -29,23 +34,23 @@ class Pad: SyntaxRewriter {
     override func visit(_ node: ClosureExprSyntax) -> ExprSyntax {
         var node = node
         if node.signature != nil {
-            node.leftBrace.trailingTrivia = .space
-            node.signature?.trailingTrivia = .newline
+            node.leftBrace.trailingTrivia.ensureWhitespace()
+            node.signature?.trailingTrivia.ensureNewline()
         } else {
-            node.leftBrace.trailingTrivia = .newline
+            node.leftBrace.trailingTrivia.ensureNewline()
         }
         return super.visit(node)
     }
 
     override func visit(_ node: FunctionCallExprSyntax) -> ExprSyntax {
         var node = node
-        node.trailingClosure?.leadingTrivia = .space
+        node.trailingClosure?.leadingTrivia.ensureWhitespace()
         return super.visit(node)
     }
 
     override func visit(_ node: TryExprSyntax) -> ExprSyntax {
         var node = node
-        node.tryKeyword.trailingTrivia = .space
+        node.tryKeyword.trailingTrivia.ensureWhitespace()
         return super.visit(node)
     }
 
@@ -53,9 +58,10 @@ class Pad: SyntaxRewriter {
 
     override func visit(_ node: ForStmtSyntax) -> StmtSyntax {
         var node = node
-        node.forKeyword.trailingTrivia = .space
-        node.inKeyword.leadingTrivia = .space
-        node.inKeyword.trailingTrivia = .space
+        node.forKeyword.trailingTrivia.ensureWhitespace()
+        node.inKeyword.leadingTrivia.ensureWhitespace()
+        node.inKeyword.trailingTrivia.ensureWhitespace()
+        node.body.leadingTrivia.ensureWhitespace()
         return super.visit(node)
     }
 
@@ -63,38 +69,40 @@ class Pad: SyntaxRewriter {
 
     override func visit(_ node: ArrayElementSyntax) -> ArrayElementSyntax {
         var node = node
-        node.trailingComma?.trailingTrivia = .space
+        node.trailingComma?.trailingTrivia.ensureWhitespace()
         return super.visit(node)
     }
 
     override func visit(_ node: AttributeSyntax) -> AttributeSyntax {
         var node = node
-        node.trailingTrivia = .space
+        node.trailingTrivia.ensureWhitespace()
         return super.visit(node)
     }
 
     override func visit(_ node: ClosureShorthandParameterSyntax) -> ClosureShorthandParameterSyntax {
         var node = node
-        node.trailingComma?.trailingTrivia = .space
+        node.trailingComma?.trailingTrivia.ensureWhitespace()
         return super.visit(node)
     }
 
     override func visit(_ node: CodeBlockItemSyntax) -> CodeBlockItemSyntax {
-        super.visit(node.with(\.trailingTrivia, .newline))
+        var node = node
+        node.trailingTrivia.ensureNewline()
+        return super.visit(node)
     }
 
     override func visit(_ node: FunctionParameterSyntax) -> FunctionParameterSyntax {
         var node = node
-        node.secondName?.leadingTrivia = .space
-        node.colon.trailingTrivia = .space
-        node.trailingComma?.trailingTrivia = .space
+        node.secondName?.leadingTrivia.ensureWhitespace()
+        node.colon.trailingTrivia.ensureWhitespace()
+        node.trailingComma?.trailingTrivia.ensureWhitespace()
         return super.visit(node)
     }
 
     override func visit(_ node: LabeledExprSyntax) -> LabeledExprSyntax {
         var node = node
-        node.colon?.trailingTrivia = .space
-        node.trailingComma?.trailingTrivia = .space
+        node.colon?.trailingTrivia.ensureWhitespace()
+        node.trailingComma?.trailingTrivia.ensureWhitespace()
         return super.visit(node)
     }
 
@@ -104,7 +112,7 @@ class Pad: SyntaxRewriter {
 
     override func visit(_ node: PatternBindingSyntax) -> PatternBindingSyntax {
         var node = node
-        node.trailingComma?.trailingTrivia = .space
+        node.trailingComma?.trailingTrivia.ensureWhitespace()
         return super.visit(node)
     }
 
@@ -112,53 +120,53 @@ class Pad: SyntaxRewriter {
 
     override func visit(_ node: ClosureSignatureSyntax) -> ClosureSignatureSyntax {
         var node = node
-        node.inKeyword.leadingTrivia = .space
+        node.inKeyword.leadingTrivia.ensureWhitespace()
         return super.visit(node)
     }
 
     override func visit(_ node: CodeBlockSyntax) -> CodeBlockSyntax {
         var node = node
-        node.leftBrace.trailingTrivia = .newline
+        node.leftBrace.trailingTrivia.ensureNewline()
         return super.visit(node)
     }
 
     override func visit(_ node: FunctionEffectSpecifiersSyntax) -> FunctionEffectSpecifiersSyntax {
         var node = node
-        node.asyncSpecifier?.leadingTrivia = .space
-        node.throwsSpecifier?.leadingTrivia = .space
+        node.asyncSpecifier?.leadingTrivia.ensureWhitespace()
+        node.throwsSpecifier?.leadingTrivia.ensureWhitespace()
         return super.visit(node)
     }
 
     override func visit(_ node: InheritanceClauseSyntax) -> InheritanceClauseSyntax {
         var node = node
-        node.colon.leadingTrivia = .space
-        node.colon.trailingTrivia = .space
+        node.colon.leadingTrivia.ensureWhitespace()
+        node.colon.trailingTrivia.ensureWhitespace()
         return super.visit(node)
     }
 
     override func visit(_ node: InitializerClauseSyntax) -> InitializerClauseSyntax {
         var node = node
-        node.equal.leadingTrivia = .space
-        node.equal.trailingTrivia = .space
+        node.equal.leadingTrivia.ensureWhitespace()
+        node.equal.trailingTrivia.ensureWhitespace()
         return super.visit(node)
     }
 
     override func visit(_ node: MemberBlockSyntax) -> MemberBlockSyntax {
         var node = node
-        node.leftBrace.trailingTrivia = .newline
+        node.leftBrace.trailingTrivia.ensureNewline()
         return super.visit(node)
     }
 
     override func visit(_ node: ReturnClauseSyntax) -> ReturnClauseSyntax {
         var node = node
-        node.arrow.leadingTrivia = .space
-        node.arrow.trailingTrivia = .space
+        node.arrow.leadingTrivia.ensureWhitespace()
+        node.arrow.trailingTrivia.ensureWhitespace()
         return super.visit(node)
     }
 
     override func visit(_ node: TypeAnnotationSyntax) -> TypeAnnotationSyntax {
         var node = node
-        node.colon.trailingTrivia = .space
+        node.colon.trailingTrivia.ensureWhitespace()
         return super.visit(node)
     }
 }
