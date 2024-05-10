015a1ccac5c4f8789cab7a82697f30639489576c
Benjamin Davies <bentendavies@gmail.com>
2024-05-10 21:57:37 +1200

    Remove isLastInParent helper func

--- Sources/Rewriters/Shrink.swift
+++ Sources/Rewriters/Shrink.swift
@@ -19,35 +19,55 @@ class Shrink: SyntaxRewriter {
 
     // Collections
 
-    override func visit(_ node: ArrayElementSyntax) -> ArrayElementSyntax {
-        super.visit(node
-            .with(\.trailingComma, isLastInParent(node) ? nil : .commaToken()))
+    override func visit(_ node: ArrayElementListSyntax) -> ArrayElementListSyntax {
+        var node = node
+        for index in node.indices {
+            if node.index(after: index) == node.endIndex {
+                node[index].trailingComma = nil
+            } else {
+                node[index].trailingComma = .commaToken()
+            }
+        }
+        return super.visit(node)
     }
 
-    override func visit(_ node: ClosureParameterSyntax) -> ClosureParameterSyntax {
-        super.visit(node
-            .with(\.trailingComma, isLastInParent(node) ? nil : .commaToken()))
+    override func visit(_ node: ClosureParameterListSyntax) -> ClosureParameterListSyntax {
+        var node = node
+        for index in node.indices {
+            if node.index(after: index) == node.endIndex {
+                node[index].trailingComma = nil
+            } else {
+                node[index].trailingComma = .commaToken()
+            }
+        }
+        return super.visit(node)
     }
 
     override func visit(_ node: CodeBlockItemSyntax) -> CodeBlockItemSyntax {
         super.visit(node.with(\.semicolon, nil))
     }
 
-    override func visit(_ node: LabeledExprSyntax) -> LabeledExprSyntax {
-        super.visit(node
-            .with(\.trailingComma, isLastInParent(node) ? nil : .commaToken()))
+    override func visit(_ node: LabeledExprListSyntax) -> LabeledExprListSyntax {
+        var node = node
+        for index in node.indices {
+            if node.index(after: index) == node.endIndex {
+                node[index].trailingComma = nil
+            } else {
+                node[index].trailingComma = .commaToken()
+            }
+        }
+        return super.visit(node)
     }
 
     override func visit(_ node: MemberBlockItemSyntax) -> MemberBlockItemSyntax {
         super.visit(node.with(\.semicolon, nil))
     }
 
-    override func visit(_ node: PatternBindingSyntax) -> PatternBindingSyntax {
-        super.visit(node
-            .with(\.trailingComma, isLastInParent(node) ? nil : .commaToken()))
+    override func visit(_ node: PatternBindingListSyntax) -> PatternBindingListSyntax {
+        var node = node
+        for index in node.indices {
+            node[index].trailingComma = index == node.indices.last ? nil : .commaToken()
+        }
+        return super.visit(node)
     }
-}
-
-fileprivate func isLastInParent(_ node: some SyntaxProtocol) -> Bool {
-    node.lastToken(viewMode: .all)?.id == node.parent?.lastToken(viewMode: .all)?.id
 }
