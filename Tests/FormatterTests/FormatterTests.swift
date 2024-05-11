90d76b7dc88bc1ca2c2010f84d25a5398763d35f
Benjamin Davies <bentendavies@gmail.com>
2024-05-11 16:25:46 +1200

    Refactor tests

--- Tests/FormatterTests/FormatterTests.swift
+++ Tests/FormatterTests/FormatterTests.swift
@@ -5,11 +5,12 @@ import Kea
 class FormatterTests: XCTestCase {
     func testFormatter() throws {
         let basePath = URL(fileURLWithPath: "TestData/FormatterTests")
-        for subFolder in try FileManager.default.contentsOfDirectory(at: basePath, includingPropertiesForKeys: nil) {
+
+        for subFolder in try listDirectory(basePath) {
             let canonicalFile = URL(fileURLWithPath: "Canonical.swift", relativeTo: subFolder)
             let canonical = try String(contentsOf: canonicalFile)
 
-            for sourceFile in try FileManager.default.contentsOfDirectory(at: subFolder, includingPropertiesForKeys: nil) {
+            for sourceFile in try listDirectory(subFolder) {
                 let contents = try String(contentsOf: sourceFile)
 
                 let source = Parser.parse(source: contents)
@@ -19,4 +20,8 @@ class FormatterTests: XCTestCase {
             }
         }
     }
+}
+
+func listDirectory(_ url: URL) throws -> [URL] {
+    try FileManager.default.contentsOfDirectory(at: url, includingPropertiesForKeys: nil)
 }
