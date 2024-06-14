aec2af0973144985a22ec4d1dcb19e078d07e450
Benjamin Davies <bentendavies@gmail.com>
2024-06-14 13:34:27 +1200

    Refactor to avoid named varargs

--- Sources/Kea.swift
+++ Sources/Kea.swift
@@ -5,13 +5,15 @@ public func format(_ syntax: SourceFileSyntax) -> SourceFileSyntax {
     print(indentType)
     return rewrite(
         syntax,
-        with: Shrink(),
-        Expand(),
-        Indent(),
-        ChangeIndentType(indentType: indentType))
+        with: [
+            Shrink(),
+            Expand(),
+            Indent(),
+            ChangeIndentType(indentType: indentType),
+        ])
 }
 
-func rewrite(_ syntax: SourceFileSyntax, with rewriters: SyntaxRewriter...) -> SourceFileSyntax {
+func rewrite(_ syntax: SourceFileSyntax, with rewriters: [SyntaxRewriter]) -> SourceFileSyntax {
     rewriters.reduce(syntax) { syntax, rewriter in
         rewriter.visit(syntax)
     }
