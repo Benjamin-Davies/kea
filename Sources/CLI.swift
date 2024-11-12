585dbf10f79e629ebb0e26c3f4ab8988014e17c2
Benjamin Davies <bentendavies@gmail.com>
2024-11-12 17:06:21 +1300

    feat: --dry-run flag

--- Sources/CLI.swift
+++ Sources/CLI.swift
@@ -8,12 +8,22 @@ struct Kakapo: ParsableCommand {
     @Argument(help: "The files to process.")
     var files: [String]
 
+    @Flag(
+        name: .shortAndLong,
+        help: "Prints the formatted code to the standard output, instead of overwriting the source files."
+    )
+    var dryRun: Bool = false
+
     func run() throws {
         for file in files {
             let source = try String(contentsOfFile: file)
-            let file = Parser.parse(source: source)
-            let formattedFile = format(file)
-            print(formattedFile, terminator: "")
+            let ast = Parser.parse(source: source)
+            let formattedFile = format(ast)
+            if dryRun {
+                print(formattedFile)
+            } else {
+                try formattedFile.write(toFile: file, atomically: true, encoding: .utf8)
+            }
         }
     }
 }
