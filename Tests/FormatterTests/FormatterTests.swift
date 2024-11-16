8a9838c015d77fde3b0bbb394baad905ab88ca27
Benjamin Davies <bentendavies@gmail.com>
2024-11-16 21:01:44 +1300

    refactor: sort tests into folders

--- Tests/FormatterTests/FormatterTests.swift
+++ Tests/FormatterTests/FormatterTests.swift
@@ -2,46 +2,58 @@ import Kea
 import SwiftParser
 import XCTest
 
+var totalFiles = 0
+
 class FormatterTests: XCTestCase {
     func testFormatter() throws {
         let basePath = URL(fileURLWithPath: "TestData/FormatterTests")
-
-        var totalFiles = 0
-        for subFolder in try listDirectory(basePath) {
-            let canonicalFile = URL(fileURLWithPath: "Canonical.swift", relativeTo: subFolder)
-            let canonical = try String(contentsOf: canonicalFile)
-
-            for sourceFile in try listDirectory(subFolder) {
-                let contents = try String(contentsOf: sourceFile)
-
-                let source = Parser.parse(source: contents)
-                let formatted = format(source)
-
-                XCTAssert(formatted == canonical, "Unexpected formatting for \(sourceFile.lastPathComponent)")
-                if formatted != canonical {
-                    let tempDir = TemporaryDirectory()
-                    try! canonical.write(
-                        to: tempDir.join(fileName: "Expected.swift"),
-                        atomically: true,
-                        encoding: .utf8)
-                    try! formatted.write(
-                        to: tempDir.join(fileName: "Actual.swift"),
-                        atomically: true,
-                        encoding: .utf8)
-
-                    let diff = Process()
-                    diff.currentDirectoryURL = tempDir.url
-                    diff.executableURL = URL(fileURLWithPath: "/usr/bin/diff")
-                    diff.arguments = ["-u", "Expected.swift", "Actual.swift"]
-                    try! diff.run()
-                    diff.waitUntilExit()
-                }
-
-                totalFiles += 1
-            }
-        }
+        try testDirectory(basePath)
 
         print("Tested \(totalFiles) files")
+    }
+}
+
+func testDirectory(_ dir: URL) throws {
+    for subDir in try listDirectory(dir) {
+        if subDir.hasDirectoryPath {
+            try testDirectory(subDir)
+        } else if subDir.pathComponents.last == "Canonical.swift" {
+            try testLeafDirectory(dir)
+        }
+    }
+}
+
+func testLeafDirectory(_ dir: URL) throws {
+    let canonicalFile = URL(fileURLWithPath: "Canonical.swift", relativeTo: dir)
+    let canonical = try String(contentsOf: canonicalFile)
+
+    for sourceFile in try listDirectory(dir) {
+        let contents = try String(contentsOf: sourceFile)
+
+        let source = Parser.parse(source: contents)
+        let formatted = format(source)
+
+        XCTAssert(formatted == canonical, "Unexpected formatting for \(sourceFile.lastPathComponent)")
+        if formatted != canonical {
+            let tempDir = TemporaryDirectory()
+            try! canonical.write(
+                to: tempDir.join(fileName: "Expected.swift"),
+                atomically: true,
+                encoding: .utf8)
+            try! formatted.write(
+                to: tempDir.join(fileName: "Actual.swift"),
+                atomically: true,
+                encoding: .utf8)
+
+            let diff = Process()
+            diff.currentDirectoryURL = tempDir.url
+            diff.executableURL = URL(fileURLWithPath: "/usr/bin/diff")
+            diff.arguments = ["-u", "Expected.swift", "Actual.swift"]
+            try! diff.run()
+            diff.waitUntilExit()
+        }
+
+        totalFiles += 1
     }
 }
 
